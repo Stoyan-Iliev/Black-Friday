6304c95de0f0d771943cb1dd85dc5005ec35be1d
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-10 18:09:34 +0300

    Create entities

---
+++ src/main/java/com/store/entity/Product.java
@@ -0,0 +1,69 @@
+package com.store.entity;
+
+
+public class Product {
+    private long id;
+    private String name;
+    private String type;
+    private int count;
+    private double price;
+    private double minPrice;
+    private boolean isOnSale;
+    private double discountPercent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(double minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public boolean isOnSale() {
+        return isOnSale;
+    }
+
+    public void setOnSale(boolean onSale) {
+        isOnSale = onSale;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
---
+++ src/main/java/com/store/entity/Purchase.java
@@ -0,0 +1,12 @@
+//package com.store.entity;
+//
+//import java.time.LocalDate;
+//
+//public class Purchase {
+//    private Product product;
+//    private User user;
+//    private LocalDate purchaseDate;
+//    private int quantity;
+//    private String address;
+//    private double
+//}
