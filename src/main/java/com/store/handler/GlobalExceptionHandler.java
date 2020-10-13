5567e5b5779d86b14773883cf41a499105c9f98a
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-14 00:31:43 +0300

    Add Logger

--- src/main/java/com/store/handler/GlobalExceptionHandler.java
+++ src/main/java/com/store/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.store.handler;
 
 import com.store.exception.ProductNotFoundException;
 import com.store.util.ApiError;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +19,7 @@ import static org.springframework.http.HttpStatus.*;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final String DELIMITER = ";" + System.lineSeparator();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResponseEntity<?> constraintExceptionHandler(ConstraintViolationException exception) {
@@ -41,6 +44,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = ProductNotFoundException.class)
     public ResponseEntity<?> noSuchElementExceptionHandler(ProductNotFoundException exception){
+        LOGGER.error(exception);
         return new ResponseEntity<>(getApiError(exception.getMessage(), BAD_REQUEST), BAD_REQUEST);
     }
 }
