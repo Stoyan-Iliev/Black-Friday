c526662d288850d31ba6d4b805c5739ad1de32cd
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-13 19:03:33 +0300

    Add calculation method for discount price in getProduct methods

--- src/main/java/com/store/service/ProductService.java
+++ src/main/java/com/store/service/ProductService.java
@@ -5,6 +5,7 @@ import com.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -17,20 +18,70 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-
-    public List<Product> getAllProducts(){
-        return productRepository.findAll();
-    }
-
     public Product addProduct(Product product){
         return productRepository.save(product);
     }
 
-    public List<Product> getAllProductsOnSale(){
-        return productRepository.getAllByIsOnSaleIsTrue();
-    }
-
     public List<Product> addProducts(List<Product> products){
         return productRepository.saveAll(products);
+    }
+
+    public List<Product> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        setDiscountPriceForAllProductsOnSale(products);
+        return products;
+    }
+
+    public List<Product> getAllProductsOnSale(){
+        List<Product> products = productRepository.getAllByIsOnSaleIsTrue();
+        setDiscountPriceForAllProductsOnSale(products);
+        return products;
+    }
+
+    private void setDiscountPriceForAllProductsOnSale(List<Product> products) {
+        if(isNull(products)){
+            return;
+        }
+
+        for (Product product : products) {
+            setNewPriceIfNeeded(product);
+        }
+    }
+
+    private boolean isNull(List<Product> products) {
+        return products == null;
+    }
+
+    private void setNewPriceIfNeeded(Product product) {
+        if(product.isOnSale()){
+            product.setPrice(calculateNewPrice(product));
+        }
+    }
+
+    private BigDecimal calculateNewPrice(Product product) {
+        BigDecimal newPrice = calculatePriceWithDiscount(product);
+        newPrice = ensurePriceIsNotBelowMinPrice(product, newPrice);
+        return newPrice;
+    }
+
+    private BigDecimal ensurePriceIsNotBelowMinPrice(Product product, BigDecimal newPrice) {
+        if(isNewPricePriceLessThanMinPrice(newPrice, product.getMinPrice())){
+            newPrice = product.getMinPrice();
+        }
+        return newPrice;
+    }
+
+    private boolean isNewPricePriceLessThanMinPrice(BigDecimal first, BigDecimal second) {
+        return second.compareTo(first) < 0;
+    }
+
+    private BigDecimal calculatePriceWithDiscount(Product product) {
+        return product.getPrice()
+                .subtract(getDiscountSum(product));
+    }
+
+    private BigDecimal getDiscountSum(Product product) {
+        return product.getPrice()
+                .multiply(BigDecimal.valueOf(product.getDiscountPercent() / 100));
     }
 }
