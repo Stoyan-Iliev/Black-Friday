b61d76388a427246fb71cc3dd78077985f135023
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-15 14:03:32 +0300

    Created methods to check if user exist either by username or email

--- src/main/java/com/store/repository/UserRepository.java
+++ src/main/java/com/store/repository/UserRepository.java
@@ -8,4 +8,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User getByEmail(String email);
     User getByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
