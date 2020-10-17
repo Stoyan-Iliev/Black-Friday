2078b0e22daf4f439d80352961661ab6552fea2e
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-17 21:32:08 +0300

    Added getIncome method

--- src/main/java/com/store/controller/PurchaseController.java
+++ src/main/java/com/store/controller/PurchaseController.java
@@ -2,10 +2,13 @@ package com.store.controller;
 
 import com.store.entity.Purchase;
 import com.store.payload.request.PurchaseRequest;
+import com.store.payload.response.IncomeResponse;
 import com.store.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -21,5 +24,13 @@ public class PurchaseController {
     @PostMapping()
     public ResponseEntity<Purchase> buyProducts(@RequestBody PurchaseRequest purchase){
         return ResponseEntity.ok(purchaseService.createPurchase(purchase));
+    }
+
+    @GetMapping(path = "/income")
+    public ResponseEntity<IncomeResponse> getIncome(@RequestParam String startDate, @RequestParam String endDate){
+        LocalDate startDate1 = LocalDate.parse(startDate);
+        LocalDate endDate1 = LocalDate.parse(endDate);
+
+        return ResponseEntity.ok(purchaseService.getIncomeBetween(startDate1, endDate1));
     }
 }
