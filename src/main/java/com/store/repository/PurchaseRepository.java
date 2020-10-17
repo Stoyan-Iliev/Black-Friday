3e6f14de820611e6b16e94fd026eab1e8b7996fe
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-10-17 21:32:37 +0300

    Method to find all purchases between dates

--- src/main/java/com/store/repository/PurchaseRepository.java
+++ src/main/java/com/store/repository/PurchaseRepository.java
@@ -4,6 +4,10 @@ import com.store.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+    List<Purchase> findAllByPurchaseDateBetweenOrderByPurchaseDate(LocalDate startDate, LocalDate endDate);
 }
