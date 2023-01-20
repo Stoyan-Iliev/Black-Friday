0072f9f34070b50a9ea8eaae301f591ec26bdf28
Stoyan Iliev <49573461+Stoyan-Iliev@users.noreply.github.com>
2023-01-20 23:41:23 +0200

    Fix error response

--- src/main/java/com/store/handler/GlobalExceptionHandler.java
+++ src/main/java/com/store/handler/GlobalExceptionHandler.java
@@ -8,9 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.mail.MessagingException;
 import javax.validation.ConstraintViolation;
@@ -18,6 +21,7 @@ import javax.validation.ConstraintViolationException;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Collections;
 import java.util.Set;
 
 import static org.springframework.http.HttpStatus.*;
@@ -26,13 +30,6 @@ import static org.springframework.http.HttpStatus.*;
 public class GlobalExceptionHandler {
     private static final String DELIMITER = ";" + System.lineSeparator();
     private static final Logger LOGGER = LogManager.getLogger();
-
-    @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity<?> constraintExceptionHandler(ConstraintViolationException exception) {
-        LOGGER.error(exception);
-        String message = concatViolationMessages(exception);
-        return ResponseEntity.status(BAD_REQUEST).body(getApiError(message, BAD_REQUEST));
-    }
 
     @ExceptionHandler(value = {
             CampaignNotFoundException.class,
@@ -44,7 +41,6 @@ public class GlobalExceptionHandler {
             UserAlreadyExistException.class,
             UserNotFoundException.class,
             IOException.class,
-            MethodArgumentNotValidException.class,
             MessagingException.class,
             UnsupportedEncodingException.class,
             DisabledException.class,
@@ -55,17 +51,43 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(BAD_REQUEST).body(getApiError(exception.getMessage(), BAD_REQUEST));
     }
 
-    private String concatViolationMessages(ConstraintViolationException exception) {
-        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
-
-        StringBuilder message = new StringBuilder();
-        for (ConstraintViolation<?> violation : violations) {
-            message.append(violation.getMessage())
-                    .append(DELIMITER);
+    @ExceptionHandler(value = { ConstraintViolationException.class } )
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (ConstraintViolation violation : e.getConstraintViolations()) {
+            error.getViolations().add(
+                    new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
         }
-        message.delete(message.lastIndexOf(DELIMITER), message.length());
-        return message.toString();
+        return error;
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.getViolations().add(
+                    new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return error;
+    }
+
+//    private String concatViolationMessages(ConstraintViolationException exception) {
+//        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
+//
+//        StringBuilder message = new StringBuilder();
+//        for (ConstraintViolation<?> violation : violations) {
+//            message.append(violation.getMessage())
+//                    .append(DELIMITER);
+//        }
+//        message.delete(message.lastIndexOf(DELIMITER), message.length());
+//        return message.toString();
+//    }
 
     private ApiError getApiError(String message, HttpStatus status) {
         return new ApiError(status, message);
