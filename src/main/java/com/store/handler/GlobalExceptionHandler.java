f0a7812e800675eebc1cb003550389618e15b9ae
Stoyan Iliev <49573461+Stoyan-Iliev@users.noreply.github.com>
2023-01-20 14:05:52 +0200

    Fix Sending Empty message in response when NotBlank annotation triggered

--- src/main/java/com/store/handler/GlobalExceptionHandler.java
+++ src/main/java/com/store/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,7 +39,8 @@ public class GlobalExceptionHandler {
             RoleNotFoundException.class,
             UserAlreadyExistException.class,
             UserNotFoundException.class,
-            IOException.class
+            IOException.class,
+            MethodArgumentNotValidException.class
             })
     public ResponseEntity<?> handleBadRequest(Exception exception){
         LOGGER.error(exception);
